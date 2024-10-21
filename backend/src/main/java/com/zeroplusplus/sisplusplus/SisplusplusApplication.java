badba7c94bc260e0d8d05e802bf4a5dca1685492
Ryan Yocum <rty4159@rit.edu>
2024-10-21 18:27:06 -0400

    Spring Initialization

---
+++ backend/src/main/java/com/zeroplusplus/sisplusplus/SisplusplusApplication.java
@@ -0,0 +1,13 @@
+package com.zeroplusplus.sisplusplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisplusplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisplusplusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zeroplusplus/sisplusplus/SisplusplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroplusplus.sisplusplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisplusplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
