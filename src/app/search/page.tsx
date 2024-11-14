8189ae0124ce79a29f04c98ddfa92c83160b1530
Ryan Yocum <rty4159@rit.edu>
2024-11-14 17:13:06 -0500

    init search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,58 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { redirect } from 'next/navigation';
+import Search from './Search';
 
-export default function Search() {
+const courses = [
+    {
+        id: 1,
+        name: 'SWEN 746',
+        description: 'Software Architecture',
+        credits: 15,
+        level: 7,
+        semester: 2,
+        year: 2021,
+        department: 'Computer Science',
+        prerequisites: [],
+        corequisites: [],
+    },
+    {
+        id: 2,
+        name: 'SWEN 742',
+        description: 'Software Testing',
+        credits: 15,
+        level: 7,
+        semester: 1,
+        year: 2021,
+        department: 'Computer Science',
+        prerequisites: [],
+        corequisites: [],
+    },
+];
+
+export default async function SearchPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ q: string }>;
+}) {
+    const params = await searchParams;
+    const query = params.q;
+    const filteredCourses = query
+        ? courses.filter((course) =>
+              course.name.toLowerCase().includes(query.toLowerCase())
+          )
+        : null;
+
     return (
         <div className="flex items-center mt-5 flex-col gap-5">
             <h1 className="text-3xl font-semibold">Search for a Course</h1>
-            <div className="flex w-full max-w-sm items-center space-x-2">
-                <Input type="text" placeholder="SWEN 746" />
-                <Button type="submit">Search</Button>
-            </div>
+            <Search />
+
+            {filteredCourses
+                ? filteredCourses.map((course) => (
+                      <div key={course.id}>{course.name}</div>
+                  ))
+                : null}
         </div>
     );
 }
