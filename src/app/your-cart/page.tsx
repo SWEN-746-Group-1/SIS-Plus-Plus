fc757e7ec00a5e21a2281d1c8cb5f7a14f36358e
dra7835 <dra7835@rit.edu>
2024-11-18 04:36:56 -0500

    Reformatted in preparation for prisma integration. Next, figure out why the courseSections cannot be seen.

--- src/app/your-cart/page.tsx
+++ src/app/your-cart/page.tsx
@@ -1,20 +1,22 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-
+import { prisma } from '@/lib/prisma';
+import { Cart, CourseSection } from '@prisma/client'
 const example_courses = [
     {name: "A", id: 0, prof:"Bob",date:"MWF"},
     {name: "B", id: 1, prof:"Bob",date:"TR"},
     {name: "C", id: 2, prof:"Bob",date:"MWF"}
 ];
 
-export default function Cart() {
+export default function YourCart( cart: Cart ) {
+    const contents: CourseSection[] = cart.courseSections;
     return (
         <div className="flex items-center mt-5 flex-col gap-5">
             <h1 className="text-3xl font-semibold">Your Cart</h1>
             <div className="flex w-full max-w-sm items-center space-x-2">
                 <ul>
                     {example_courses.map((course) => (
-                        <li key={course.id}>{course.name},{course.prof},{course.date}</li>))}
+                        <li key={course.id}>{course.name}, {course.prof}, {course.date}</li>))}
                 </ul>
             </div>
         </div>
