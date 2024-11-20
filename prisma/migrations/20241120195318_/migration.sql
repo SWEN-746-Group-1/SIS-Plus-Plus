666e29c0e02b9258e77becfc9e661b0104563daf
Ryan Yocum <rty4159@rit.edu>
2024-11-20 16:09:23 -0500

    removed migrations

--- prisma/migrations/20241120195318_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `catalogId` on the `CourseSection` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "CourseSection" DROP COLUMN "catalogId";
--- prisma/migrations/20241120200421_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `daysOfTheWeek` column on the `TimeSlot` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "TimeSlot" DROP COLUMN "daysOfTheWeek",
-ADD COLUMN     "daysOfTheWeek" TEXT[];
