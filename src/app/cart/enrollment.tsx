59f6d435c42685ebed482b54d0945ff3e97f219c
Reid Taylor <rbt6577@rit.edu>
2024-12-02 20:16:43 -0500

    core of validation algorithm implemented but not tested, getting unexpected prisma error

--- src/app/cart/enrollment.tsx
+++ src/app/cart/enrollment.tsx
@@ -4,10 +4,39 @@ import { auth } from '@/lib/auth';
 import { prisma } from '@/lib/prisma'
 // import { revalidatePath } from 'next/cache'
 
+interface Interval {
+    start: number;
+    end: number;
+}
+
+interface Schedule {
+    m: Interval[];
+    t: Interval[];
+    w: Interval[];
+    r: Interval[];
+    f: Interval[];
+}
+
+export interface ValidationResponse {
+    status: string;
+    notes: string[];
+}
+
+function getDay(week: Schedule, day: string) {
+    switch (day) {
+        case "m": return week.m;
+        case "t": return week.t;
+        case "w": return week.w;
+        case "r": return week.r;
+        case "f": return week.f;
+        default: return null
+    }
+}
+
 export async function validateCart() {
     const session = await auth();
 
-    let userId;
+    let userId: string | null;
     if (!session || !session.user || !session.user.id) {
         userId = null;
         throw new Error('You must be logged in to validate a cart')
@@ -40,7 +69,55 @@ export async function validateCart() {
         })
     }
 
-    cart?.courseSections.map((section) => {
-        console.log(section.course.fullCode + "." + section.section)
+    const id = userId;
+    const fullUser = await prisma.user.findFirst({
+        where: {
+            id
+        },
+        include: {
+            completedCourses: true
+        }
     })
+
+    const schedule: Schedule = {
+        m: [],
+        t: [],
+        w: [],
+        r: [],
+        f: [],
+    }
+
+    const res: ValidationResponse = {
+        status: "",
+        notes: []
+    }
+
+    cart?.courseSections.map((section) => {
+        section.timeSlot?.daysOfTheWeek.map((day) => {
+            getDay(schedule, day)?.map((interval) => {
+                if(section.timeSlot !== null) {
+
+                    if(section.timeSlot?.startTime < interval.end && 
+                        section.timeSlot.endTime > interval.start
+                    ) {
+                        if(res.status !== "invalid") {
+                            res.status = "invalid"
+                        }
+                        res.notes.push(`Scheduling conflict for ${section.course.fullCode}.${section.section}`)
+                    }
+
+                    section.course.prerequisites.map((prereq) => {
+                        if(!fullUser?.completedCourses.some((completed) => {return(completed.code == prereq.code)})) {
+                            if(res.status !== "invalid") {
+                                res.status = "invalid"
+                            }
+                            res.notes.push(`Unmet prerequisite "${prereq.fullCode}" for ${section.course.fullCode}`)
+                        }
+                    })
+                }
+            })
+        })
+    })
+
+    return res;
 }
